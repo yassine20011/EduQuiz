b5857c08fe7f7a9b6f0c5ce0958f4bb39576ccf4
yassine20011 <medalgamer47@gmail.com>
2023-01-27 17:41:46 +0100

    SSl handshake error

--- university/settings.py
+++ university/settings.py
@@ -162,9 +162,9 @@ LOGIN_REDIRECT_URL = '/dashboard'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_HOST_USER =  'no.reply.eduquiz@gmail.com'
-EMAIL_HOST_PASSWORD = 'bkkzhuokwcvjjvhw'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
